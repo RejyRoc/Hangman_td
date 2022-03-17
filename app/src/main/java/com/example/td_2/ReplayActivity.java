cedea762725cdf7b72a05a8559ddc5137ae68bc2
Rejy Roc <rocrejy>
2022-03-17 17:15:35 -0400

    lew klike sou rejwe li mache donk jwet lA JOUAB

--- app/src/main/java/com/example/td_2/ReplayActivity.java
+++ app/src/main/java/com/example/td_2/ReplayActivity.java
@@ -2,7 +2,10 @@ package com.example.td_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ReplayActivity extends AppCompatActivity {
 
@@ -11,6 +14,16 @@ public class ReplayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_replay);
 
+        Button m = findViewById(R.id.button_rejwe);
 
+        View.OnClickListener mo = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bi = new Intent(ReplayActivity.this,GameActivity.class);
+                startActivity(bi);
+            }
+        };
+
+        m.setOnClickListener(mo);
     }
 }
